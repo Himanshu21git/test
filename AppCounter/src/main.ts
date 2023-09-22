68ef0465b962b3f6a3cec5571596765e0056c3d3
Aman-kumar-vidyamantra <aman.kumar@vidyamantra.com>
2023-09-22 11:55:11 +0530

    Aman Complet remaining task

--- AppCounter/src/main.ts
+++ AppCounter/src/main.ts
@@ -6,4 +6,8 @@ import { createPinia } from 'pinia'
 
 
 
+<<<<<<< HEAD
 createApp(App).use(createPinia()).use(router).mount('#app')
+=======
+createApp(App).use(createPinia()).use(router).mount('#app')
+>>>>>>> 6824723 (Aman Complet remaining task)
