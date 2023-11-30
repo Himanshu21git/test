40235b3afaca00aa150705979a26459dacaf8dc1
Rajnish <razz.king2004@gmail.com>
2023-11-30 10:18:52 +0530

    coustom Directives

--- raj/AppCounter/src/main.ts
+++ raj/AppCounter/src/main.ts
@@ -2,4 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+
+app.directive('highlight',(el, binding)=>{
+    el.style.backgroundColor  = binding.value
+})
+
+app.mount('#app')
