f60ec752457219dfbbc80f3da0e88fba414de1ce
Rajnish <razz.king2004@gmail.com>
2023-12-11 10:41:30 +0530

    TailwindCSS Task

--- raj/AppCounter/src/App.vue
+++ raj/AppCounter/src/App.vue
@@ -4,15 +4,3 @@
 
 
 
-<script setup lang="ts">
-// import AppCounter from './components/AppCounter.vue'
-// import AppList from './components/AppList.vue';
-// import AppForm from './components/AppForm.vue';
-// import AppParent from './components/AppParent.vue';
-// import Cart from './components/Cart.vue';
-// import { RouterLink } from 'vue-router';
-
-</script>
-
-
-
--- raj/AppCounter/src/components/Home.vue
+++ raj/AppCounter/src/components/Home.vue
@@ -1,27 +1,27 @@
 <template>
-  <h3 v-highlight>Task 1 and Tast 2</h3>
-  <AppCounter />
-  <hr>
+     <h3 v-highlight>Task 1 and Tast 2</h3>
+     <AppCounter />
+     <hr>
 
-  <h3 v-highlight>Task 3 and Tast 4</h3>
-  <AppList :arr="listData" />
-  <hr>
+     <h3 v-highlight>Task 3 and Tast 4</h3>
+     <AppList :arr="listData" />
+     <hr>
 
-  <h3 v-highlight>Task 5</h3>
-  <AppForm />
+     <h3 v-highlight>Task 5</h3>
+     <AppForm />
 
-  <h3 v-highlight>Task 6 & 7</h3>
-  <li v-for="item, i in cart.productList" :key="i">
-    {{ item.Product }} = ${{ item.Price }}
-  </li>
-  <br>
-  <RouterLink :to="{ name: 'ProductDetails' }">Product Details</RouterLink>
+     <h3 v-highlight>Task 6 & 7</h3>
+     <li v-for="item, i in cart.productList" :key="i">
+          {{ item.Product }} = ${{ item.Price }}
+     </li>
+     <br>
+     <RouterLink :to="{ name: 'ProductDetails' }">Product Details</RouterLink>
 
-  <h3 v-highlight>Task 8</h3>
-  <FetchApi />
+     <h3 v-highlight>Task 8</h3>
+     <FetchApi />
 
-  <h3 v-highlight>Task 9</h3>
-  <AppParent />
+     <h3 v-highlight>Task 9</h3>
+     <AppParent />
 </template>
 
 <script setup lang="ts">
@@ -35,17 +35,17 @@ import FetchApi from './FetchApi.vue';
 const cart = useCartStore()
 
 const listData = [
-  { value: 10, editID: false },
-  { value: 20, editID: false },
-  { value: 30, editID: false },
-  { value: 40, editID: false },
+     { value: 10, id: 1 },
+     { value: 20, id: 2 },
+     { value: 30, id: 3 },
+     { value: 40, id: 4 },
 ]
 
 </script>
 
 <style scoped>
 h3 {
-  color: white;
-  text-decoration: underline;
+     color: white;
+     text-decoration: underline;
 }
 </style>
